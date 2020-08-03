e08493b52f9faea3f1603f8b4cbe44f1fcd1f883
Bryce Kennedy <btken88@gmail.com>
2020-08-03 16:49:48 -0600

    Create navigation functionality in starter app.

--- rn-starter/src/screens/HomeScreen.js
+++ rn-starter/src/screens/HomeScreen.js
@@ -1,8 +1,17 @@
 import React from "react";
-import { Text, StyleSheet } from "react-native";
+import { Text, StyleSheet, View, Button } from "react-native";
+import { TouchableOpacity } from "react-native-gesture-handler";
 
-const HomeScreen = () => {
-  return <Text style={styles.text}>What's up?! Is it working on both? Maybe...</Text>;
+const HomeScreen = ({ navigation }) => {
+  return (
+    <View>
+      <Text style={styles.text}>What's up?! Is it working on both? Maybe...</Text>
+      <Button
+        title="Go to Components Demo"
+        onPress={() => navigation.navigate('Components')} />
+      <Button onPress={() => navigation.navigate('List')} title="Go to List Demo" />
+    </View>
+  )
 };
 
 const styles = StyleSheet.create({
